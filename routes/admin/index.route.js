8b7f2cafef1b7c31d07fd338f8ff4c5305cd13d9
Dương Nhật Minh <toilaminhhh@gmail.com>
2024-11-07 09:35:06 +0700

    project

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,7 @@
 const systemConfig = require("../../config/system")
 
+const authMidlleware = require("../../middlewares/admin/auth.middleware")
+
 const dashboardRoutes = require("./dashboard.route")
 const productRoutes = require("./product.route")
 const productCategoryRoutes = require("./product-category.route")
@@ -10,15 +12,19 @@ const authRoutes = require("./auth.route")
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
 
-    app.use(PATH_ADMIN + "/dashboard", dashboardRoutes)
+    app.use(
+        PATH_ADMIN + "/dashboard", 
+        authMidlleware.requireAuth, 
+        dashboardRoutes
+    )
 
-    app.use(PATH_ADMIN + "/products", productRoutes)
+    app.use(PATH_ADMIN + "/products", authMidlleware.requireAuth, productRoutes)
 
-    app.use(PATH_ADMIN + "/products-category", productCategoryRoutes)
+    app.use(PATH_ADMIN + "/products-category", authMidlleware.requireAuth, productCategoryRoutes)
 
-    app.use(PATH_ADMIN + "/roles", roleRoutes)
+    app.use(PATH_ADMIN + "/roles", authMidlleware.requireAuth, roleRoutes)
 
-    app.use(PATH_ADMIN + "/accounts", accountRoutes)
+    app.use(PATH_ADMIN + "/accounts", authMidlleware.requireAuth, accountRoutes)
 
     app.use(PATH_ADMIN + "/auth", authRoutes)
-}    
+}
