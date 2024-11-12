6e42c0146189ffb34d2b6d5a9713e5986a90b9e7
Dương Nhật Minh <toilaminhhh@gmail.com>
2024-11-12 17:17:57 +0700

    project

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -7,8 +7,6 @@ module.exports.index = async (req, res) => {
         deleted: false 
     }).sort({position: "desc"})
 
-    // console.log(products)
-
     const newProducts = products.map(item => {
         item.priceNew = ((item.price * (100 - item.discountPercentage)) / 100).toFixed(0);
         return item;
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,12 @@
+const CategoryMiddleware = require("../../middlewares/client/category.middleware")
+
 const homeRoutes = require("./home.route")
 const productRoutes = require("./product.route")
 
+
 module.exports = (app) => {
+    app.use(CategoryMiddleware.category)
+
     app.use('/', homeRoutes)
  
     app.use('/products', productRoutes)
